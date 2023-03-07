b31e1ab7ddb2ed3bf915756b99cfa083c7a77681
Josue-Alberto <101291362+Josue-Alberto@users.noreply.github.com>
2023-03-07 15:38:57 -0400

    finalizado
    Ya termine con todo

--- Sistema de deudas/Form1.Designer.cs
+++ Sistema de deudas/Form1.Designer.cs
@@ -47,6 +47,13 @@
             this.sistemadeDeudasBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.deudasDataSet1 = new Sistema_de_deudas.DeudasDataSet1();
             this.sistema_de_DeudasTableAdapter = new Sistema_de_deudas.DeudasDataSet1TableAdapters.Sistema_de_DeudasTableAdapter();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtiddelete = new System.Windows.Forms.TextBox();
+            this.sistema_de_DeudasTableAdapter1 = new Sistema_de_deudas.TransporteMinyetyDataSet1TableAdapters.Sistema_de_DeudasTableAdapter();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.sistemadeDeudasBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.deudasDataSet1)).BeginInit();
@@ -54,9 +61,9 @@
             // 
             // btnGuardar
             // 
-            this.btnGuardar.Location = new System.Drawing.Point(100, 386);
+            this.btnGuardar.Location = new System.Drawing.Point(157, 422);
             this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(89, 38);
+            this.btnGuardar.Size = new System.Drawing.Size(129, 38);
             this.btnGuardar.TabIndex = 31;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = true;
@@ -64,17 +71,18 @@
             // 
             // btnCerrar
             // 
-            this.btnCerrar.Location = new System.Drawing.Point(12, 386);
+            this.btnCerrar.Location = new System.Drawing.Point(12, 422);
             this.btnCerrar.Name = "btnCerrar";
-            this.btnCerrar.Size = new System.Drawing.Size(82, 38);
+            this.btnCerrar.Size = new System.Drawing.Size(129, 38);
             this.btnCerrar.TabIndex = 32;
             this.btnCerrar.Text = "Cerrar";
             this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 344);
+            this.label6.Location = new System.Drawing.Point(18, 329);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(37, 13);
             this.label6.TabIndex = 24;
@@ -83,7 +91,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(9, 168);
+            this.label4.Location = new System.Drawing.Point(19, 182);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(104, 13);
             this.label4.TabIndex = 25;
@@ -92,16 +100,17 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 63);
+            this.label2.Location = new System.Drawing.Point(19, 77);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 13);
             this.label2.TabIndex = 26;
             this.label2.Text = "Nombre";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(9, 221);
+            this.label5.Location = new System.Drawing.Point(19, 232);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(104, 13);
             this.label5.TabIndex = 27;
@@ -110,7 +119,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 112);
+            this.label3.Location = new System.Drawing.Point(19, 126);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(49, 13);
             this.label3.TabIndex = 28;
@@ -119,7 +128,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 11);
+            this.label1.Location = new System.Drawing.Point(19, 26);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(40, 13);
             this.label1.TabIndex = 29;
@@ -127,53 +136,57 @@
             // 
             // txtNumero
             // 
-            this.txtNumero.Location = new System.Drawing.Point(12, 184);
+            this.txtNumero.Location = new System.Drawing.Point(21, 198);
             this.txtNumero.Name = "txtNumero";
             this.txtNumero.Size = new System.Drawing.Size(177, 20);
             this.txtNumero.TabIndex = 21;
             // 
             // txtApellido
             // 
-            this.txtApellido.Location = new System.Drawing.Point(12, 128);
+            this.txtApellido.Location = new System.Drawing.Point(21, 142);
             this.txtApellido.Name = "txtApellido";
             this.txtApellido.Size = new System.Drawing.Size(177, 20);
             this.txtApellido.TabIndex = 18;
             // 
             // txtMonto
             // 
-            this.txtMonto.Location = new System.Drawing.Point(12, 360);
+            this.txtMonto.Location = new System.Drawing.Point(21, 345);
             this.txtMonto.Name = "txtMonto";
             this.txtMonto.Size = new System.Drawing.Size(177, 20);
             this.txtMonto.TabIndex = 22;
             // 
             // txtDetalle
             // 
-            this.txtDetalle.Location = new System.Drawing.Point(12, 237);
+            this.txtDetalle.Location = new System.Drawing.Point(21, 248);
             this.txtDetalle.Multiline = true;
             this.txtDetalle.Name = "txtDetalle";
-            this.txtDetalle.Size = new System.Drawing.Size(254, 94);
+            this.txtDetalle.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDetalle.Size = new System.Drawing.Size(254, 67);
             this.txtDetalle.TabIndex = 19;
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(12, 79);
+            this.txtNombre.Location = new System.Drawing.Point(21, 93);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(177, 20);
             this.txtNombre.TabIndex = 23;
             // 
             // txtCedula
             // 
-            this.txtCedula.Location = new System.Drawing.Point(12, 28);
+            this.txtCedula.Location = new System.Drawing.Point(21, 42);
             this.txtCedula.Name = "txtCedula";
             this.txtCedula.Size = new System.Drawing.Size(177, 20);
             this.txtCedula.TabIndex = 20;
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(292, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(545, 450);
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(545, 486);
             this.dataGridView1.TabIndex = 33;
             // 
             // sistemadeDeudasBindingSource
@@ -190,11 +203,83 @@
             // 
             this.sistema_de_DeudasTableAdapter.ClearBeforeFill = true;
             // 
+            // btnEliminar
+            // 
+            this.btnEliminar.ForeColor = System.Drawing.Color.Red;
+            this.btnEliminar.Location = new System.Drawing.Point(12, 466);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(274, 32);
+            this.btnEliminar.TabIndex = 34;
+            this.btnEliminar.Text = "Eliminar un registor";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(204, 13);
+            this.label7.TabIndex = 36;
+            this.label7.Text = "Selecciona el id de el que quieres eliminar";
+            this.label7.Visible = false;
+            // 
+            // txtiddelete
+            // 
+            this.txtiddelete.Location = new System.Drawing.Point(21, 27);
+            this.txtiddelete.Name = "txtiddelete";
+            this.txtiddelete.Size = new System.Drawing.Size(177, 20);
+            this.txtiddelete.TabIndex = 35;
+            this.txtiddelete.Visible = false;
+            // 
+            // sistema_de_DeudasTableAdapter1
+            // 
+            this.sistema_de_DeudasTableAdapter1.ClearBeforeFill = true;
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(21, 51);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(75, 23);
+            this.btndelete.TabIndex = 37;
+            this.btndelete.Text = "Eliminar";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Visible = false;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(123, 51);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 38;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Visible = false;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(204, 25);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 39;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Visible = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(849, 474);
+            this.ClientSize = new System.Drawing.Size(849, 510);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtiddelete);
+            this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnGuardar);
             this.Controls.Add(this.btnCerrar);
@@ -241,6 +326,13 @@
         private DeudasDataSet1 deudasDataSet1;
         private System.Windows.Forms.BindingSource sistemadeDeudasBindingSource;
         private DeudasDataSet1TableAdapters.Sistema_de_DeudasTableAdapter sistema_de_DeudasTableAdapter;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtiddelete;
+        private TransporteMinyetyDataSet1TableAdapters.Sistema_de_DeudasTableAdapter sistema_de_DeudasTableAdapter1;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnBuscar;
     }
 }
 
--- Sistema de deudas/Form1.cs
+++ Sistema de deudas/Form1.cs
@@ -21,10 +21,10 @@ namespace Sistema_de_deudas
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            SqlConnection connection = new SqlConnection("server=JMGAMER15\\SQLEXPRESS; database=Deudas; integrated security=true");
+            SqlConnection connection = new SqlConnection("Server=tcp:srvtransportes.database.windows.net,1433;Initial Catalog=SistemaDeudas;Persist Security Info=False;User ID=transporteMinAdmin;Password=TransporteMinyetypsw012@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30");
             connection.Open();
             // TODO: esta línea de código carga datos en la tabla 'deudasDataSet1.Sistema_de_Deudas' Puede moverla o quitarla según sea necesario.
-            String codigo3 = "Select * From Sistema_de_Deudas ";
+            String codigo3 = "Select * from Deudas ";
             SqlCommand comando3 = new SqlCommand(codigo3, connection);
             SqlDataReader reader = comando3.ExecuteReader();
             connection.Close();
@@ -40,41 +40,172 @@ namespace Sistema_de_deudas
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
+            
             String Cedula = txtCedula.Text;
             String Nombre = txtNombre.Text;
             String Apellido = txtApellido.Text;
             String Numero = txtNumero.Text;
             String Detalles = txtDetalle.Text;
             String Monto = txtMonto.Text;
+            float monto_pagado = 0;
 
+            if (Cedula == "" || Nombre == "" || Apellido == "" || Numero =="" || Detalles == "" || Monto=="") {
 
-            SqlConnection connection = new SqlConnection("server=JMGAMER15\\SQLEXPRESS; database=Deudas; integrated security=true");
+                MessageBox.Show("Debe llenar todos los campos!!!");
+            }
+            else { 
+            SqlConnection connection = new SqlConnection("Server=tcp:srvtransportes.database.windows.net,1433;Initial Catalog=SistemaDeudas;Persist Security Info=False;User ID=transporteMinAdmin;Password=TransporteMinyetypsw012@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30");
             connection.Open();
-            String codigo = "insert into Sistema_de_Deudas(Cedula,Nombre,Apellidos,Numero,Detalles,Monto) values('" + Cedula + "','" + Nombre + "','" + Apellido + "','" + Numero + "','" + Detalles + "','" + Monto + "')";
+            String codigo = "insert into Deudas(Cedula,Nombre,Apellidos,telefono,Detalles,Monto,Monto_Pagado) values('" + Cedula + "','" + Nombre + "','" + Apellido + "','" + Numero + "','" + Detalles + "','" + Monto + "','" + monto_pagado + "')";
             SqlCommand comando = new SqlCommand(codigo, connection);
             SqlDataReader reader3 = comando.ExecuteReader();
-
+            txtCedula.Clear();
+            txtNombre.Clear();
+            txtApellido.Clear();
+            txtNumero.Clear();
+            txtDetalle.Clear();
+            txtMonto.Clear();
             connection.Close();
 
 
             connection.Open();
-
-
-            String codigo2 = "Select * From Sistema_de_Deudas ";
+            String codigo2 = "Select * From Deudas ";
             SqlCommand comando2 = new SqlCommand(codigo2, connection);
             SqlDataReader reader = comando2.ExecuteReader();
             connection.Close();
+
+
             connection.Open();
             SqlDataAdapter da = new SqlDataAdapter(comando2);
             DataTable dt = new DataTable();
             da.Fill(dt);
             dataGridView1.DataSource = dt;
-
-
-
-
-
             connection.Close();
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+
+            //quitar visivilidad
+            label1.Visible= false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+    
+            txtApellido.Visible = false;
+            txtCedula.Visible = false;
+            txtDetalle.Visible = false;
+            txtMonto.Visible = false;
+            txtNombre.Visible = false;
+            txtNumero.Visible = false;
+       
+            btnCerrar.Visible = false;
+            btnGuardar.Visible = false;
+            btnEliminar.Visible = false;
+
+
+            //poner visible
+            txtiddelete.Visible = true;
+            label7.Visible= true;
+            btnOK.Visible= true;
+            btndelete.Visible= true;
+            btnBuscar.Visible= true;
+
+
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            String id1 = txtiddelete.Text;
+            int id = int.Parse(id1);
+            SqlConnection connection = new SqlConnection("Server=tcp:srvtransportes.database.windows.net,1433;Initial Catalog=SistemaDeudas;Persist Security Info=False;User ID=transporteMinAdmin;Password=TransporteMinyetypsw012@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30");
+            connection.Open();
+            // TODO: esta línea de código carga datos en la tabla 'deudasDataSet1.Sistema_de_Deudas' Puede moverla o quitarla según sea necesario.
+            String codigo3 = "Delete from Deudas where ID='"+id+"' ";
+            SqlCommand comando3 = new SqlCommand(codigo3, connection);
+            SqlDataReader reader = comando3.ExecuteReader();
+            connection.Close();
+            connection.Open();
+            SqlDataAdapter da1 = new SqlDataAdapter(comando3);
+            DataTable dt1 = new DataTable();
+            da1.Fill(dt1);
+            dataGridView1.DataSource = dt1;
+            connection.Close();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            //quitar visivilidad
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+            label7.Visible = true;
+
+            txtApellido.Visible = true;
+            txtCedula.Visible = true;
+            txtDetalle.Visible = true;
+            txtMonto.Visible = true;
+            txtNombre.Visible = true;
+            txtNumero.Visible = true;
+
+            btnCerrar.Visible = true;
+            btnGuardar.Visible = true;
+            btnEliminar.Visible= true;
+            
+
+
+            //poner visible
+            txtiddelete.Visible = false;
+            label7.Visible = false;
+            btnOK.Visible = false;
+            btndelete.Visible = false;
+            btnBuscar.Visible= false;
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            String dato = txtiddelete.Text;
+            if (dato == "") {
+
+                MessageBox.Show("No hay ningun dato en la tabla!!!");
+            }
+            else {
+                String id1 = txtiddelete.Text;
+                int id = int.Parse(id1);
+            
+               
+
+            SqlConnection connection = new SqlConnection("Server=tcp:srvtransportes.database.windows.net,1433;Initial Catalog=SistemaDeudas;Persist Security Info=False;User ID=transporteMinAdmin;Password=TransporteMinyetypsw012@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30");
+            connection.Open();
+            // TODO: esta línea de código carga datos en la tabla 'deudasDataSet1.Sistema_de_Deudas' Puede moverla o quitarla según sea necesario.
+            String codigo3 = "Select * from Deudas where ID='" + id + "' ";
+            SqlCommand comando3 = new SqlCommand(codigo3, connection);
+            SqlDataReader reader = comando3.ExecuteReader();
+            connection.Close();
+            connection.Open();
+            SqlDataAdapter da1 = new SqlDataAdapter(comando3);
+            DataTable dt1 = new DataTable();
+            da1.Fill(dt1);
+            dataGridView1.DataSource = dt1;
+            connection.Close();
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
