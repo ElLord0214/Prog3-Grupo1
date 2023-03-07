c46496db6b1d672225f545ec393b8d0415db3736
Josue-Alberto <101291362+Josue-Alberto@users.noreply.github.com>
2023-03-06 22:56:18 -0400

    botones_interfaz
    Agregue los botones a la interfaz

--- Sistema de deudas/Form2.cs
+++ Sistema de deudas/Form2.cs
@@ -16,5 +16,17 @@ namespace Sistema_de_deudas
         {
             InitializeComponent();
         }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'transporteMinyetyDataSet.Sistema_de_Deudas' Puede moverla o quitarla según sea necesario.
+            this.sistema_de_DeudasTableAdapter.Fill(this.transporteMinyetyDataSet.Sistema_de_Deudas);
+
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
